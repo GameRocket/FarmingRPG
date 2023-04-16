706a550258934d473f6bf79e48061cae065f227c
rengetsu <rengetsu@mail.ru>
2023-04-16 23:28:59 +0300

    Finding player game object

--- FarmingRPG/Assets/Scripts/CameraController.cs
+++ FarmingRPG/Assets/Scripts/CameraController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //  Player transform component
+    Transform playerPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //  Find the player game object in the scene and gets its transform component
+        playerPos = FindObjectOfType<PlayerController>().transform;
     }
 
     // Update is called once per frame
