0e0781805cab2447a9886bd653f8aa2417fffca3
rengetsu <rengetsu@mail.ru>
2023-04-17 18:49:42 +0300

    Finish Basic Camera

--- FarmingRPG/Assets/Scripts/CameraController.cs
+++ FarmingRPG/Assets/Scripts/CameraController.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Camera Controller
+/// </summary>
 public class CameraController : MonoBehaviour
 {
+    public float offsetZ = 5f;
+    public float smoothing = 2f;
+
     //  Player transform component
     Transform playerPos;
 
@@ -17,6 +23,16 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FollowPlayer();
+    }
+
+    // Following the player
+    void FollowPlayer()
+    {
+        //  Position the camera should be in
+        Vector3 targetPosition = new Vector3(playerPos.position.x, transform.position.y, playerPos.position.z - offsetZ);
+
+        //  Set the position accordingly
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
     }
 }
