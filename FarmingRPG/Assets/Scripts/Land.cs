23b27195a3530da4dbdffcbaa03affe8d054305b
Pavel Trostianko <rengetsu@mail.ru>
2023-06-06 20:35:58 +0300

    LandStatus and Mud materials variables

--- FarmingRPG/Assets/Scripts/Land.cs
+++ FarmingRPG/Assets/Scripts/Land.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Land : MonoBehaviour
 {
+    public enum LandStatus
+    {
+        Dry, Brown, Leaves
+    }
+
+    /// <summary>
+    /// Mud material types
+    /// dryMudMat - soilMat (Dry)
+    /// brownMudMat - farmlandMat (Brown)
+    /// leavesMudMat - wateredMat (Leaves)
+    /// </summary>
+    public Material dryMudMat, brownMudMat, leavesMudMat;
+
     // Start is called before the first frame update
     void Start()
     {
