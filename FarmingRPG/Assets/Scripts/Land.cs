697095e09d157b170e9209914998285cb8cc79ca
Pavel Trostianko <rengetsu@mail.ru>
2023-06-07 20:13:18 +0300

    Method for switch land material

--- FarmingRPG/Assets/Scripts/Land.cs
+++ FarmingRPG/Assets/Scripts/Land.cs
@@ -9,6 +9,8 @@ public class Land : MonoBehaviour
         Dry, Brown, Leaves
     }
 
+    public LandStatus landStatus;
+
     /// <summary>
     /// Mud material types
     /// dryMudMat - soilMat (Dry)
@@ -16,16 +18,46 @@ public class Land : MonoBehaviour
     /// leavesMudMat - wateredMat (Leaves)
     /// </summary>
     public Material dryMudMat, brownMudMat, leavesMudMat;
+    new Renderer renderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //  Get the renderer component
+        renderer = GetComponent<Renderer>();
+
+        //  Set the land to dry by default
+        SwitchLandStatus(LandStatus.Dry);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    ///  Method to handle a land status
+    /// </summary>
+    /// <param name="statusToSwitch"></param>
+    public void SwitchLandStatus(LandStatus statusToSwitch)
     {
-        
+        landStatus = statusToSwitch;
+
+        Material materialToSwitch = dryMudMat ;
+
+        // Decide what material to switch to and reflect the changes on our cube
+        switch(statusToSwitch)
+        {
+            case LandStatus.Dry:
+                //  Switch to the dry material
+                materialToSwitch = dryMudMat;
+                break;
+            case LandStatus.Brown:
+                //  Switch to the brown material
+                materialToSwitch = brownMudMat;
+                break;
+            case LandStatus.Leaves:
+                //  Switch to the leaves material
+                materialToSwitch = leavesMudMat;
+                break;
+        }
+
+        //  Get the renderer to apply the changes
+        renderer.material = materialToSwitch;
     }
 }
