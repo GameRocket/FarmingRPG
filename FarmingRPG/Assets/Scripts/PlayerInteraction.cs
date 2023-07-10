99c08c5177b2022ee9a34ff2808351c329167842
NightShifterMind <pavel.trostianko@outlook.com>
2023-07-10 22:17:10 +0300

    OnInteractableHit method created

--- FarmingRPG/Assets/Scripts/PlayerInteraction.cs
+++ FarmingRPG/Assets/Scripts/PlayerInteraction.cs
@@ -12,12 +12,28 @@ public class PlayerInteraction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //  Get access to our PlayerController component
+        playerController = transform.parent.parent.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //  This will be our hit info (whatever raycast hits)
+        RaycastHit hit;
+
+        //  Checking if our player interacts with something
+        if(Physics.Raycast(transform.position, Vector3.down, out hit, 1))
+        {
+            OnInteractableHit(hit);
+        }    
+    }
+
+    /// <summary>
+    /// Method called when the player hit something that he can interact with
+    /// </summary>
+    void OnInteractableHit(RaycastHit hit)
+    {
+        Debug.Log(hit);
     }
 }
